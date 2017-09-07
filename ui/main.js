f750bc4068b5018a68a4630879b30489397a69f5
SuperdudeSudhir <sdhrsingh271@gmail.com>
2017-09-08 00:23:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,24 +8,23 @@ button.onclick = function()
     var request = new XMLHttpRequest();
     
     //CAPTURE RESPONSE
-    request.onreadystatechange = function ()
-    {
-    if (request.readyState === XMLHttpRequest.DONE)
+    //request.onreadystatechange = function ()
+   // {
+    //if (request.readyState === XMLHttpRequest.DONE)
     
-    {
+   // {
         //RENDER IN CORRECTT SPAN
         
         
-        if(request.status === 200){
-counter = 1;
-counter =  request.responseText;
+       // if(request.status === 200){
+//counter =  request.responseText;
 var span = document.getElementById('count');
 span.innerHTML = counter.toString();
-    }
-    
-    }
-};
+ //   }
+  //  
+   // }
+//};
 
-request.open('GET','http://sdhrsingh271.imad.hasura-app.io/',true);
-request.send(null);
+//request.open('GET','http://sdhrsingh271.imad.hasura-app.io/',true);
+//request.send(null);
 };
