143612ed75d67e5df7611adb2f0a959339816d86
SuperdudeSudhir <sdhrsingh271@gmail.com>
2017-09-17 19:29:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,6 +19,20 @@ PASSWORD:'1042616258s'
 };
 
 var pool =new Pool(config);
+app.get('/test-db',function(req,res)
+    {
+        
+        pool.query('SELECT *FROM test' ,function(err,result){
+            
+         if(err){
+             res.status(500).send(err.toString());
+             
+         }   
+         else{
+             res.send(JSON.stringify(result));
+     }
+    });
+});
 
 
 
